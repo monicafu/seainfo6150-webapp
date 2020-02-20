f28ee8b448bdbb6820c0aee3e55c22c81ac64a94
monicafu <31578677+monicafu@users.noreply.github.com>
2020-02-20 11:42:05 -0800

    week-7 assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -7,9 +7,7 @@ const ArticleList = props => {
   return (
     <ul className={styles.container}>
       {props.articles.map(article => (
-        <li key={article.slug}>
-          <ArticleListItem article={article} />
-        </li>
+        <ArticleListItem key={article.slug} article={article}/>
       ))}
     </ul>
   );
--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -5,15 +5,15 @@ import ArticleImage from "./ArticleImage";
 
 const ArticleListItem = props => {
   return (
-    <div className={styles.container}>
+    <li className={styles.container}>
       <div className={styles.articleImage}><ArticleImage title={props.article.title} url={props.article.image._url}/></div>
-        <article className={styles.article}>
-          <h3 className={styles.title}>{props.article.title}</h3>
-          <p>{props.article.shortText}</p>
-          <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-        </article>
-          <SlugButton className= {styles.slugButton} slug={props.article.slug} buttonText={props.article.author} />
-    </div>
+      <article className={styles.article}>
+        <h3 className={styles.title}>{props.article.title}</h3>
+        <p>{props.article.shortText}</p>
+        <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
+      </article>
+      <SlugButton className= {styles.slugButton} slug={props.article.slug} buttonText={props.article.author} />
+    </li>
   );
 };
 
