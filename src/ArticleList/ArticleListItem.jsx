7ca746d00fdcc4ad342d8875a49312b484482261
monicafu <31578677+monicafu@users.noreply.github.com>
2020-02-19 23:41:11 -0800

    week-7 assignment

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -7,14 +7,12 @@ const ArticleListItem = props => {
   return (
     <div className={styles.container}>
       <div className={styles.articleImage}><ArticleImage title={props.article.title} url={props.article.image._url}/></div>
-      <div className={styles.content}>
         <article className={styles.article}>
           <h3 className={styles.title}>{props.article.title}</h3>
           <p>{props.article.shortText}</p>
           <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
         </article>
           <SlugButton className= {styles.slugButton} slug={props.article.slug} buttonText={props.article.author} />
-      </div>
     </div>
   );
 };
